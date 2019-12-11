3e592dd77d7ef28e43307bbe16d3f456d21ba29f
ShadowSneaker <33228779+ShadowSneaker@users.noreply.github.com>
2019-12-11 15:15:04 +0000

    More Vector.

--- CopiriteMath/CopiriteMath/CopiriteMath/Datatypes/Vector.h
+++ CopiriteMath/CopiriteMath/CopiriteMath/Datatypes/Vector.h
@@ -271,6 +271,103 @@ public:
 
 	// Operator, Returns the vector component value at the given axis.
 	INLINE Type operator[](const EAxis& Axis) const;
+
+
+
+	/// Debug
+
+	// Debug diagnostics handle for when a vector contains NaN in any component.
+	// @note - This will set this vector to a vector0 if it contains NaN.
+	INLINE void CheckNaN() const;
+
+	// Check if this vector's components contains NaN.
+	// @return - True if a component contains NaN.
+	INLINE bool ContainsNaN() const;
+
+	// Prints out the contents of this vector to the console.
+	INLINE void Print() const;
+
+
+
+	/// Conversions
+
+#ifdef INCLUDE_DIRECTX_MATH
+	// Converts a DirectX::XMVECTOR to this type of vector.
+	// @param V - The XMVECTOR to convert.
+	// @return - The converted vector.
+	static INLINE STVector<Size, Type> FromXMVector(DirectX::XMVECTOR Vector);
+
+	// Converts this vector to a DirectX::XMVECTOR.
+	INLINE DirectX::XMVECTOR ToXMVector() const;
+#endif // INCLUDE_DIRECTX_MATH
+
+	// Converts this vector to represent a rotation of unit length.
+	INLINE STVector<3, float> Rotation() const;
+
+	// Converts this vector to a specified type.
+	// @template NewType - The new datatype this vector should be.
+	template <typename NewType>
+	INLINE STVector<Size, NewType> ToType();
+
+	// Converts this vector to a specified type.
+	// @template NewType - The new datatype this vector should be.
+	template <typename NewType>
+	INLINE STVector<Size, NewType> ToType() const;
+
+	// Converts this vector to a floating point vector.
+	INLINE STVector<Size, float> ToFloat();
+
+	// Converts this vector to a floating point vector.
+	INLINE STVector<Size, float> ToFloat() const;
+
+	// Converts this vector to a double type vector.
+	INLINE STVector<Size, double> ToDouble();
+
+	// Converts this vector to a double type vector.
+	INLINE STVector<Size, double> ToDouble() const;
+
+	// Converts this vector to an integer vector.
+	INLINE STVector<Size, int32> ToInt();
+
+	// Converts this vector to an integer vector.
+	INLINE STVector<Size, int32> ToInt() const;
+
+
+
+	/// Functions
+
+	// Calculates the cross product between this vector and another vector.
+	// @param Other - The inputted vector to calculate against.
+	// @return - The resulting vector.
+	INLINE STVector<3, Type> CrossProduct(const STVector<3, Type>& Other) const;
+
+	// Calculates the dot product between thsi vector an another vector.
+	// @param Other - The inputted vector to calculate against.
+	// @return - The resulting vector.
+	INLINE float DotProduct(const STVector<Size, Type>& Other) const;
+
+	// Creates a vector with the highest values in each dimension between this vector and an inputted vector.
+	// @param Other - The inputted vector to calculate against.
+	// @return - The resulting vector.
+	INLINE STVector<Size, Type> Max(const STVector<Size, Type>& Other) const;
+
+	// Creates a vector with the lowest values in each dimension between this vector and an inputted vector.
+	// @param Other - The inputted vector to calculate against.
+	// @return - The resulting vector.
+	INLINE STVector<Size, Type> Min(const STVector<Size, Type>& Other) const;
+
+	// Normalize the vector.
+	// @param Tolerance - The accuracy towards teh normalization.
+	INLINE void Normalize(flaot Tolerance = MICRO_NUMBER);
+
+	// Returns true if this vector is almost equal to another vector.
+	// @param Other - The vector to compare with.
+	// @param Threshold - The range in which the other vector can be in.
+	// @return - Returns true if the other vector is within range of this vector.
+	INLINE bool nearlyEqual(const STVector<Size, Type>& Other, const Type& Threshold = MICRO_NUMBER) const;
+
+	// Checks to see if this vector is close to zero based on a range.
+	// @param Range - The 
 };
 
 
@@ -582,3 +679,469 @@ INLINE STVector<Size, Type> STVector<Size, Type>::operator*(const Type& Value) c
 	Result.CheckNaN();
 	return Result;
 }
+
+
+template <uint Size, typename Type>
+template <uint Size2, typename Type2>
+INLINE STVector<Size, Type> STVector<Size, Type>::operator*=(const STVector<Size2, Type2>& Other)
+{
+	uint Count{ (Size < Size2) ? Size : Size };
+	for (uint i = 0; i < Count; ++i)
+	{
+		Data[i] *= Other[i];
+	}
+	CheckNaN();
+	return *this;
+}
+
+
+template <uint Size, typename Type>
+INLINE STVector<Size, Type> STVector<Size, Type>::operator*=(const Type& Value)
+{
+	for (uint i = 0; i < Size; ++i)
+	{
+		Data[i] *= Value;
+	}
+	CheckNaN();
+	return *this;
+}
+
+
+template <uint Size, typename Type>
+template <uint Size2, typename Type2>
+INLINE STVector<Size, Type> STVector<Size, Type>::operator/(const STVector<Size2, Type2>& Other) const
+{
+	uint Count{ (Size < Size2) ? Size : Size2 };
+	STVector<Size, Type> Result;
+	for (uint i = 0; i < Count; ++i)
+	{
+		Result[i] = Data[i] / Other[i];
+	}
+	Result.CheckNaN();
+	return Result;
+}
+
+
+template <uint Size, typename Type>
+INLINE STVector<Size, Type> STVector<Size, Type>::operator/(const Type& Value) const
+{
+	STVector<Size, Type> Result;
+	for (uint i = 0; i < Count; ++i)
+	{
+		Result[i] = Data[i] / Value;
+	}
+	Result.CheckNaN();
+	return Result;
+}
+
+
+template <uint Size, typename Type>
+template <uint Size2, typename Type2>
+INLINE STVector<Size, Type> STVector<Size, Type>::operator/=(const STVector<Size2, Type2>& Other)
+{
+	uint Count{ (Size < Size2) ? Size : Size2 };
+	for (uint i = 0; i < Count; ++i)
+	{
+		Data[i] /= Other[i];
+	}
+	CheckNaN();
+	return *this;
+}
+
+
+template <uint Size, typename Type>
+INLINE STVector<Size, Type> STVector<Size, Type>::operator/=(const Type& Value)
+{
+	for (uint i = 0; i < Size; ++i)
+	{
+		Data[i] /= Value;
+	}
+	CheckNaN();
+	return *this;
+}
+
+
+template <uint Size, typename Type>
+INLINE STVector<Size, Type> STVector<Size, Type>::operator++()
+{
+	for (uint i = 0; i < Size; ++i)
+	{
+		++Data[i];
+	}
+	CheckNaN();
+	return *this;
+}
+
+
+template <uint Size, typename Type>
+INLINE STVector<Size, Type> STVector<Size, Type>::operator--()
+{
+	for (uint i = 0; i < Size; ++i)
+	{
+		--Data[i];
+	}
+	CheckNaN();
+	return *this;
+}
+
+
+template <uint Size, typename Type>
+INLINE STVector<Size, Type> STVector<Size, Type>::operator=(const Type& Value)
+{
+	for (uint i = 0; i < Size; ++i)
+	{
+		Data[i] = Value;
+	}
+	CheckNaN();
+	return *this;
+}
+
+
+template <uint Size, typename Type>
+INLINE STVector<Size, Type> STVector<Size, Type>::operator|(const STVector<Size, Type>& Other) const
+{
+	STVector<Size, Type> Result{ 0.0f };
+	Result[EAxis::X] = (Data[EAxis::Y] * Other[EAxis::Z) - (Data[EAxis::Z] * Other[EAxis::Y]);
+	Result[EAxis::Y] = (Data[EAxis::Z] * Other[EAxis::X) - (Data[EAxis::X] * Other[EAxis::Z]);
+	Result[EAxis::Z] = (Data[EAxis::X] * Other[EAxis::Y) - (Data[EAxis::Y] * Other[EAxis::X]);
+	Result.CheckNaN();
+	return Result;
+}
+
+
+template <uint Size, typename Type>
+INLINE Type STVector<Size, Type>::operator^(const STVector<Size, Type>& Other) const
+{
+	Type Result{ 0.0f };
+	for (uint i = 0; i < Size; ++i)
+	{
+		Result += Data[i] * Other[i];
+	}
+	if (!TMath::IsFinite(Result)) Result = 0.0f;
+	return Result;
+}
+
+
+template <uint Size, typename Type>
+INLINE bool STVector<Size, Type>::operator>(const STVector<Size, Type>& Other) const
+{
+	for (uint i = 0; i < Size; ++i)
+	{
+		if (Data[i] <= Other[i]) return false;
+	}
+	return true;
+}
+
+
+template <uint Size, typename Type>
+INLINE bool STVector<Size, Type>::operator>(const Type& Value) const
+{
+	for (uint i = 0; i < Size; ++i)
+	{
+		if (Data[i] <= Value) return false;
+	}
+	return true;
+}
+
+
+template <uint Size, typename Type>
+INLINE bool STVector<Size, Type>::operator>=(const STVector<Size, Type>& Other) const
+{
+	for (uint i = 0; i < Size; ++i)
+	{
+		if (Data[i] < Other[i]) return false;
+	}
+	return true;
+}
+
+
+template <uint Size, typename Type>
+INLINE bool STVector<Size, Type>::operator>=(const Type& Value) const
+{
+	for (uint i = 0; i < Size; ++i)
+	{
+		if (Data[i] < Value) return false;
+	}
+	return true;
+}
+
+
+template <uint Size, typename Type>
+INLINE bool STVector<Size, Type>::operator<(const STVector<Size, Type>& Other) const
+{
+	for (uint i = 0; i < Size; ++i)
+	{
+		if (Data[i] >= Other[i]) return false;
+	}
+	return true;
+}
+
+
+template <uint Size, typename Type>
+INLINE bool STVector<Size, Type>::operator<(const Type& Value) const
+{
+	for (uint i = 0; i < Size; ++i)
+	{
+		if (Data[i] >= Value) return false;
+	}
+	return true;
+}
+
+
+template <uint Size, typename Type>
+INLINE bool STVector<Size, Type>::operator<=(const STVector<Size, Type>& Other) const
+{
+	for (uint i = 0; i < Size; ++i)
+	{
+		if (Data[i] > Other[i]) return false;
+	}
+	return true;
+}
+
+
+template <uint Size, typename Type>
+INLINE bool STVector<Size, Type>::operator<=(const Type& Other) const
+{
+	for (uint i = 0; i < Size; ++i)
+	{
+		if (Data[i] > Value) return false;
+	}
+	return true;
+}
+
+
+template <uint Size, typename Type>
+INLINE bool STVector<Size, Type>::operator==(const STVector<Size, Type>& Other) const
+{
+	for (uint i = 0; i < Size; ++i)
+	{
+		if (Data[i] != Other[i]) return false;
+	}
+	return true;
+}
+
+
+template <uint Size, typename Type>
+INLINE bool STVector<Size, Type>::operator==(const Type& Value) const
+{
+	for (uint i = 0; i < Size; ++i)
+	{
+		if (Data[i] != Value) return false;
+	}
+	return true;
+}
+
+
+template <uint Size, typename Type>
+INLINE bool STVector<Size, Type>::operator!=(const STVector<Size, Type>& Other) const
+{
+	for (uint i = 0; i < Size; ++i)
+	{
+		if (Data[i] == Value) return false;
+	}
+	return true;
+}
+
+
+template <uint Size, typename Type>
+INLINE bool STVector<Size, Type>::operator!=(const Type& Value) const
+{
+	for (uint i = 0; i < Size; ++i)
+	{
+		if (Data[i] == Value) return false;
+	}
+	return true;
+}
+
+
+template <uint Size, typename Type>
+INLINE Type& STVector<Size, Type>::operator[](const uint& Index)
+{
+	return Data[Index];
+}
+
+
+template <uint Size, typename Type>
+INLINE Type STVector<Size, Type>::operator[](const uint& Index) const
+{
+	return Data[Index];
+}
+
+
+template <uint Size, typename Type>
+INLINE Type& STVector<Size, Type>::operator[](const EAxis& Axis)
+{
+	return Data[Axis];
+}
+
+
+template <uint Size, typename Type>
+INLINE Type STVector<Size, Type>::operator[](const EAxis& Axis) const
+{
+	return Data[Axis];
+}
+
+
+template <uint Size, typename Type>
+INLINE void STVector<Size, Type>::CheckNaN() const
+{
+	if (ContainsNaN())
+	{
+		printf("Vector contains NaN\n");
+		*const_cast<Vector<Size, Type>*>(this) = Vector<Size, Type>{ 0.0f };
+	}
+}
+
+
+template <uint Size, typename Type>
+INLINE bool STVector<Size, Type>::ContainsNaN() const
+{
+	for (uint i = 0; i < Size; ++i)
+	{
+		if (!TMath::IsFinite(Data[i])) return true;
+	}
+	return false;
+}
+
+
+template <uint Size, typename Type>
+INLINE void STVector<Size, Type>::Print() const
+{
+	for (uint i = 0; i < Size; ++i)
+	{
+		if (i - 1 == Size) printf("%f\n", Data[i]);
+		else printf("%f, ", Data[i]);
+	}
+}
+
+
+#ifdef INCLUDE_DIRECTX_MATH
+template <uint Size, typename Type>
+INLINE STVector<Size, Type> STVector<Size, Type>::FromXMVector(DirectX::XMVECTOR Vector)
+{
+	STVector<Size, Element> Result;
+	switch (Size)
+	{
+	default:
+	case 4:
+		Result[3] = DirectX::XMVectorGetW(Vector);
+
+	case 3:
+		Result[2] = DirectX::XMVectorGetZ(Vector);
+
+	case 2:
+		Result[1] = DirectX::XMVectorGetY(Vector);
+
+	case 1:
+		Result[0] = DirectX::XMVectorGetX(Vector);
+	}
+	return Result;
+}
+
+
+template <uint Size, typename Type>
+INLINE DirectX::XMVECTOR STVector<Size, Type>::ToXMVector() const
+{
+	switch (Size)
+	{
+	case 0:
+		return DirectX::XMVectorSet(0.0f, 0.0f, 0.0f, 0.0f);
+
+	case 1:
+		return DirectX::XMVectorSet(Data[0], 0.0f, 0.0f, 0.0f);
+
+	case 2:
+		return DirectX::XMVectorSet(Data[0], Data[1], 0.0f, 0.0f);
+
+	case 3:
+		return DirectX::XMVectorSet(Data[0], Data[1], Data[2], 0.0f);
+
+	case 4:
+	default:
+		return DirectX::XMVectorSet(Data[0], Data[1], Data[2], Data[3]);
+	}
+}
+#endif // INCLUDE_DIRECTX_MATH
+
+
+template <uint Size, typename Type>
+INLINE STVector<3, float> STVector<Size, Type>::Rotation() const
+{
+	ASSERT(Size >= 3, "Vector must have 3 or more dimenions to do this conversion.");
+	Result[EAxis::Y] = TO_DEGREES(TMath::ATan2(Data[EAxis::Y], Data[EAxis::X]));
+	Result[EAxis::X] = TO_DEGREES(TMath::ATan2(Data[EAxis::Z], TMath::Sqrt((Data[EAxis::X * Data[EAxis::X]) + (Data[EAxis::Y] * Data[EAxis::Y]))));
+	Result[EAxis::Z] = 0.0f;
+	return Result;
+}
+
+
+template <uint Size, typename Type>
+template <typename NewType>
+INLINE STVector<Size, NewType> STVector<Size, Type>::ToType()
+{
+	STVector<Size, Type> Result;
+	for (uint i = 0; i < Size; ++i)
+	{
+		Result[i] = (Type)Data[i];
+	}
+	Result.CheckNaN();
+	return Result;
+}
+
+
+template <uint Size, typename Type>
+template <typename NewType>
+INLINE STVector<Size, NewType> STVector< Size, Type>::ToType() const
+{
+	STVector<Size, Type> Result;
+	for (uint i = 0; i < Size; ++i)
+	{
+		Result[i] = (Type)Data[i];
+	}
+	Result.CheckNaN();
+	return Result;
+}
+
+
+template <uint Size, typename Type>
+INLINE STVector<Size, float> STVector<Size, Type>::ToFloat()
+{
+	return ToType<float>();
+}
+
+
+template <uint Size, typename Type>
+INLINE STVector<Size, float> STVector<Size, Type>::ToFloat() const
+{
+	return ToType<float>();
+}
+
+
+template <uint Size, typename Type>
+INLINE STVector<Size, double> STVector<Size, Type>::ToDouble()
+{
+	return ToType<double>();
+}
+
+
+template <uint Size, typename Type>
+INLINE STVector<Size, double> STVector<Size, Type>::ToDouble() const
+{
+	return ToType<double>();
+}
+
+
+template <uint Size, typename Type>
+INLINE STVector<Size, int32> STVector<Size, Type>::ToInt()
+{
+	return ToType<int32>();
+}
+
+
+template <uint Size, typename Type>
+INLINE STVector<Size, int32> STVector<Size, Type>::ToInt() const
+{
+	return ToType<int32>();
+}
+
+
